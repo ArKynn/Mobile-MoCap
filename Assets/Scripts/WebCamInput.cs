d0e986d49ae3c3eb7fb49208fdee07a2464114cd
ArKynn <david.f.mendes@proton.me>
2025-10-30 11:05:45 +0000

    Removed unused Visualizers, image and canvas scaler now adapt to currently active webcam device, removed old RotateImage method

--- Assets/Scripts/WebCamInput.cs
+++ Assets/Scripts/WebCamInput.cs
@@ -9,14 +9,11 @@ public class WebCamInput : MonoBehaviour
     [SerializeField] string webCamName;
     [SerializeField] Vector2 webCamResolution = new Vector2(1080, 1920);
     [SerializeField] RawImage image;
-
-    // Provide input image Texture.
-    public Texture InputImageTexture => inputRT;
+    [SerializeField] CanvasScaler scaler;
 
     private WebCamTexture _webCamTexture;
-    private Texture2D _tempTexture;
+    public WebCamTexture WebCamTexture => _webCamTexture;
     public RenderTexture inputRT;
-    [FormerlySerializedAs("visualizerInputRT")] public RenderTexture imageInputRT;
     
     private WebCamDevice[] _devices;
     private int _currentDevice;
@@ -30,10 +27,11 @@ public class WebCamInput : MonoBehaviour
 
     void Start()
     {
-        inputRT = new RenderTexture((int)webCamResolution.x, (int)webCamResolution.y, 0);
-        imageInputRT = new RenderTexture((int)webCamResolution.y, (int)webCamResolution.x, 0);
+        inputRT = new RenderTexture((int)webCamResolution.x, (int)webCamResolution.y, 0)
+        {
+            enableRandomWrite = true
+        };
         InitializeWebcam();   
-        _tempTexture = new Texture2D(_webCamTexture.width, _webCamTexture.height, TextureFormat.RGBA32, false);
     }
 
     void InitializeWebcam()
@@ -41,6 +39,7 @@ public class WebCamInput : MonoBehaviour
         _devices = WebCamTexture.devices;
         _webCamTexture = new WebCamTexture();
         UpdateWebcam();
+        UpdateRawImage();
     }
 
     public void NextWebcamDevice()
@@ -52,6 +51,7 @@ public class WebCamInput : MonoBehaviour
         else _currentDevice = 0;
         
         UpdateWebcam();
+        UpdateRawImage();
     }
     
     void UpdateWebcam()
@@ -63,61 +63,23 @@ public class WebCamInput : MonoBehaviour
         _webCamTexture.Play();
     }
 
+    void UpdateRawImage()
+    {
+        scaler.referenceResolution = new Vector2(_webCamTexture.requestedWidth, _webCamTexture.requestedHeight);
+        image.rectTransform.sizeDelta = new Vector2(_webCamTexture.requestedWidth, _webCamTexture.requestedHeight);
+    }
+
     void Update()
     {
         if(!_webCamTexture.didUpdateThisFrame) return;
-
-        _tempTexture.SetPixels(_webCamTexture.GetPixels());
-        _tempTexture.Apply();
         
-        //_tempTexture = RotateTexture(_tempTexture, false);
-        RotateImage(_tempTexture, 90f);
+        Graphics.Blit(_webCamTexture, inputRT); 
         
-        Graphics.Blit(_webCamTexture, imageInputRT);
-        Graphics.Blit(_tempTexture, inputRT); 
-        
-        image.texture = imageInputRT;
+        image.texture = inputRT;
     }
 
     void OnDestroy(){
         if (_webCamTexture != null) Destroy(_webCamTexture);
         if (inputRT != null) Destroy(inputRT);
-    }
-    
-    public static void RotateImage(Texture2D tex, float angleDegrees)
-    {
-        int width = tex.width;
-        int height = tex.height;
-        float halfHeight = height * 0.5f;
-        float halfWidth = width * 0.5f;
-
-        var texels = tex.GetRawTextureData<Color32>();
-        var copy = System.Buffers.ArrayPool<Color32>.Shared.Rent(texels.Length);
-        Unity.Collections.NativeArray<Color32>.Copy(texels, copy, texels.Length);
-
-        float phi = Mathf.Deg2Rad * angleDegrees;
-        float cosPhi = Mathf.Cos(phi);
-        float sinPhi = Mathf.Sin(phi);
-
-        int address = 0;
-        for (int newY = 0; newY < height; newY++)
-        {
-            for (int newX = 0; newX < width; newX++)
-            {
-                float cX = newX - halfWidth;
-                float cY = newY - halfHeight;
-                int oldX = Mathf.RoundToInt(cosPhi * cX + sinPhi * cY + halfWidth);
-                int oldY = Mathf.RoundToInt(-sinPhi * cX + cosPhi * cY + halfHeight);
-                bool InsideImageBounds = (oldX > -1) & (oldX < width)
-                                                     & (oldY > -1) & (oldY < height);
-
-                texels[address++] = InsideImageBounds ? copy[oldY * width + oldX] : default;
-            }
-        }
-
-        // No need to reinitialize or SetPixels - data is already in-place.
-        tex.Apply(true);
-
-        System.Buffers.ArrayPool<Color32>.Shared.Return(copy);
     }
 }
